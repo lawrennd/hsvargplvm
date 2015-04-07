fb260d6e5d35737a8b9fa7ee96bfb5ea0c8af73f
Neil Lawrence <lawrennd@gmail.com>
2015-04-07 13:00:27 +0100

    Added hsvargplvmToolboxes.m

---
+++ matlab/hsvargplvmToolboxes.m
@@ -0,0 +1,15 @@
+% HSVARGPLVMTOOLBOXES Load in the relevant toolboxes for hierarchical sharedvariational gplvm.
+
+importTool('vargplvm');
+importLatest('fgplvm');
+importLatest('gplvm');
+importLatest('netlab');
+importLatest('prior');
+importLatest('optimi');
+importLatest('datasets');
+importTool('mltools');
+importLatest('kern');
+importLatest('ndlutil');
+importLatest('mocap');
+importLatest('gp');
+importLatest('noise');
